77e60276a560680aa55eb1cb155486e457c3a224
cesar <cesar.ago2@gmail.com>
2019-04-20 22:02:52 -0500

    NFC Reader activity reads NFC Tags!

--- Contador/app/src/main/java/com/example/contador/MainActivity.kt
+++ Contador/app/src/main/java/com/example/contador/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.contador
 
 import android.content.Intent
+import android.nfc.NfcAdapter
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
@@ -13,11 +15,22 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_menu)
 
         val btnOpenList: Button = findViewById(R.id.list_button)
+        val btnNfc: Button = findViewById(R.id.nfc_button)
 
         btnOpenList.setOnClickListener {
             val intent = Intent(this, CompleteListActivity::class.java)
             startActivity(intent)
         }
+
+        btnNfc.setOnClickListener {
+            val intent = Intent(this, NfcActivity::class.java)
+            startActivity(intent)
+        }
+
+
+        var nfcAdapter = NfcAdapter.getDefaultAdapter(this)
+        Log.d("NFC supported", (nfcAdapter != null).toString())
+        Log.d("NFC enabled", (nfcAdapter?.isEnabled).toString())
     }
 
 
